90289877000b5ff0d134731bacc0303d27fc9542
Teenar20 <tinamthembu123@gmail.com>
2023-09-12 12:04:17 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,20 @@
  * main - Program that prints _putchar, followed by a new line
  * Return:0 (Succiess)
  */
+
 int main(void)
+
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+
 }
